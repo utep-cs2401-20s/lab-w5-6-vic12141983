b2a1004781eacd544e872af1dce3094447ab188d
victor <vherreravi@miners.uteo.edu>
2020-02-24 14:50:34 -0700

    Made Modifications to exhaustive tail method. Completed reset method Started helper method have to reevaulate

--- snake/src/SnakeGame.java
+++ snake/src/SnakeGame.java
@@ -5,6 +5,7 @@ public class SnakeGame {
     private static int recursiveChecks;
 
     public SnakeGame() {
+
         game = new boolean[1][1];
     }
     public SnakeGame(boolean[][]array,int x, int y){
@@ -24,30 +25,67 @@ public class SnakeGame {
 
 
     }
-    public int[] findTailExhaustive(){
-        // temp array to keep error message from code
-        int [] array = {1,2};
-        int checks = 0;
-        for(int i =0;i<game.length;i++){
-            for(int j=0;j<game[i].length;j++){
+    public int [] lengthCounter(){
+        int lengthSnake =0;
+        for( int i =0;i<game.length;i++){
+            for( int j=0;j<game[i].length;j++){
                 if(game[i][j]=true){
-                    checks++;
+                    lengthSnake++;
+
                 }
             }
         }
+
+
+    }
+    public int[] findTailExhaustive(){
+        //1. Reset counters to zero
+        resetCounters();
+        int [] array = new int [3];
+        //2. Counter to keep track of checks.
+
+        // Traverse game
+        for(int i =0;i<game.length;i++){
+            for(int j=0;j<game[i].length;j++){
+        //3.Checks if is snake
+                if(game[i][j]=true){
+        //4. Condition to keep checks within the game
+                  if(i-1>=0&&i-1<game.length-1&&j-1>=0&&j-1<game.length-1){
+        //5. First check of neighbors
+                        if(game[i-1][j]){
+
+                        }
+                    }
+                }
+            }
+            // do checks to for head or tail position.
+        }
+        array[2] = lengthSnake;
         // find location of each next block step by step.
         return array;
     }
+    public int nextNeigbor(int x, int y){
+        if([x+1]==0){
+
+
+        }
+
+    }
+
+
+
     public int[] findTailRecursive(){
         // same
      int []arr = new int[2];
     return arr;
     }
     private void resetCounters(){
+        exhaustiveChecks =0;
+        recursiveChecks =0;
         // reset both methods to zero.
 
     }
-    private stati int getRecursiveChecks(){
+    private static int getRecursiveChecks(){
 
         return -1;
     }
