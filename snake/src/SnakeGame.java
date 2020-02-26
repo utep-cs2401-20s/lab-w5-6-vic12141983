f4617fefa6d71b0f66809d7c22a7357a3f46beee
victor <vherreravi@miners.uteo.edu>
2020-02-26 14:43:37 -0700

    Made changes to tail exhaustive began tail recursive again.

--- snake/src/SnakeGame.java
+++ snake/src/SnakeGame.java
@@ -1,6 +1,6 @@
 public class SnakeGame {
-    private boolean [][]game;
-    private int [] headPosition;
+    private boolean[][] game;
+    private int[] headPosition;
     private static int exhaustiveChecks;
     private static int recursiveChecks;
 
@@ -8,15 +8,16 @@ public class SnakeGame {
 
         game = new boolean[1][1];
     }
-    public SnakeGame(boolean[][]array,int x, int y){
-        for(int i =0;i<array.length;i++){
-            for(int j=0;j<array[i].length;j++){
-                game[i][j]= array[i][j];
+
+    public SnakeGame(boolean[][] array, int x, int y) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                game[i][j] = array[i][j];
             }
         }
         headPosition = new int[2];
-        headPosition[0]=x;
-        headPosition[1]=y;
+        headPosition[0] = x;
+        headPosition[1] = y;
 
 
 /// check the length of x and y to mark the head.
@@ -25,74 +26,102 @@ public class SnakeGame {
 
 
     }
-    public int [] lengthCounter(){
-        int lengthSnake =0;
-        for( int i =0;i<game.length;i++){
-            for( int j=0;j<game[i].length;j++){
-                if(game[i][j]=true){
+
+    // 1,helper method that keeps track of the length of snake.
+    public int[] lengthCounter() {
+        // 2
+        int lengthSnake = 0;
+        int[] array = new int[3];
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game[i].length; j++) {
+                if (game[i][j] == true) {
                     lengthSnake++;
-
-                }
-            }
-        }
-
-
-    }
-    public int[] findTailExhaustive(){
-        //1. Reset counters to zero
-        resetCounters();
-        int [] array = new int [3];
-        //2. Counter to keep track of checks.
-
-        // Traverse game
-        for(int i =0;i<game.length;i++){
-            for(int j=0;j<game[i].length;j++){
-        //3.Checks if is snake
-                if(game[i][j]=true){
-        //4. Condition to keep checks within the game
-                  if(i-1>=0&&i-1<game.length-1&&j-1>=0&&j-1<game.length-1){
-        //5. First check of neighbors
-                        if(game[i-1][j]){
-
-                        }
+                    if (game[i - 1][j] || game[i + 1][j]) {
+                        lengthSnake++;
                     }
                 }
             }
+        }
+        return array;
+
+    }
+
+    public int[] findTailExhaustive() {
+        //1. Reset counters to zero
+        resetCounters();
+        int length=0;
+        int neighbors =0;
+        //2. Array size 3 which holds position of tail,exhaustive checks and length of snake.
+        int[] array = new int[3];
+
+
+        //3. Traverse game
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game[i].length; j++) {
+        //4.Checks if the current position is snake?
+        // If position is true do not increment the exhaustiveChecks.
+                if (game[i][j] = true) {
+
+                    length++;
+        //5. Check if the current position is not head
+
+                        if ((i - 1 > -1) && (j - 1 > -1) && (i + 1 <= game.length - 1) && (j + 1 <= game.length - 1)) {
+        //7. check number of neighbors
+                            if (game[i - 1][j] || game[i + 1][j]) {
+                                neighbors++;
+
+                            }
+
+                            if(game[i][j-1]||game[i][j+1]) {
+                                neighbors++;
+
+                            }
+                            length+=neighbors;// adds to the length of the snake.
+                            if (i != headPosition[0]&&j!=headPosition[1]&& neighbors==1) {
+                                array[0] = i;
+                                array[1] = j;
+                            }
+
+                        }
+
+                }
+                else if(game[i][j]= false) {
+                    exhaustiveChecks++;
+                }
+//                }else{
+//                    exhaustiveChecks--;
+//                }
+            }
             // do checks to for head or tail position.
         }
-        array[2] = lengthSnake;
+
         // find location of each next block step by step.
+        array[2]=length;
         return array;
     }
-    public int nextNeigbor(int x, int y){
-        if([x+1]==0){
 
 
-        }
-
-    }
-
-
-
-    public int[] findTailRecursive(){
+    public int[] findTailRecursive() {
         // same
-     int []arr = new int[2];
-    return arr;
+        int[] arr = new int[2];
+        return arr;
     }
-    private void resetCounters(){
-        exhaustiveChecks =0;
-        recursiveChecks =0;
+
+    private void resetCounters() {
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
         // reset both methods to zero.
 
     }
-    private static int getRecursiveChecks(){
+
+    private static int getRecursiveChecks() {
 
         return -1;
     }
-    private static int getExhaustiveChecks(){
+
+    private static int getExhaustiveChecks() {
         return -1;
     }
-
 
 
 }
